d867057b0a8c7d766410ed01a4547bcbc9efe8a8
“Tangdabao” <1813354283@qq.com>
2017-09-24 15:40:33 +0800

    Upadate

--- Test.c
+++ Test.c
@@ -1,7 +1,22 @@
 #include"studio.h"
+
+void delay(int i);
+
 int main(void)
 {
   printf("This is a C Program.\n");
   printf("This is a C Program. V1.0\n");
   return 0;  
 }
+
+void delay (int i)
+{
+ for(i=0;i<100;i++)
+ {
+	__NOP(); 
+	 
+ }	 
+	
+	
+	 
+  }
--- Test.h
+++ Test.h
@@ -1,4 +1,4 @@
 #ifndef _Test_h
 #define _Test_h
-
+void delay(int i);
 #endif
